262cadc699b0b06f652d0a5584a6801fe0619858
KlaudiaOst <80642219+KlaudiaOst@users.noreply.github.com>
2021-05-30 12:38:42 +0200

    Up flip

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,10 +7,17 @@ public class PlayerMovement : MonoBehaviour {
     public float jumpForce = 800;
 
     private Rigidbody2D rb;
+
     private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
     private Animator animator;
     private float prev;
     private float current;
+
+    private bool facingRight = true;
 
 
     // Start is called before the first frame update
@@ -24,40 +31,30 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
+        
+
         float xDisplacement = Input.GetAxis("Horizontal") * speed;
         rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
         animator.SetFloat("speed", rb.velocity.x);
 
-        
-
-        //current = transform.position.y;
-
-        //if (current < prev)
-        //{
-        //    animator.SetTrigger("fall");
-        //}
-
-        prev = current;
-
-        /*if (rb.velocity.y < 0 && !isGrounded) 
+        if (facingRight == false && xDisplacement > 0)
         {
-            animator.SetTrigger("fall");
-        }
-        */
-        //Debug.Log(rb.velocity.y);
-
-
-        //flip
-        Vector3 characterScale = transform.localScale;
-        if (Input.GetAxis("Horizontal") < 0)
+            Flip();
+        } else if (facingRight == true && xDisplacement <0)
         {
-            characterScale.x = -1;
+            Flip();
         }
-        if (Input.GetAxis("Horizontal") > 0)
+
+            prev = current;
+
+        void Flip()
         {
-            characterScale.x = 1;
+            facingRight = !facingRight;
+            Vector3 Scaler = transform.localScale;
+            Scaler.x *= -1;
+            transform.localScale = Scaler;
         }
-        transform.localScale = characterScale;
 
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
