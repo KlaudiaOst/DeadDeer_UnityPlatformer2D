6f4f5ce10a3104859efdd988e5299be72added9d
KlaudiaOst <80642219+KlaudiaOst@users.noreply.github.com>
2021-06-19 14:02:56 +0200

    Up przed big zmianą

--- Assets/Scripts/Scene/ResetScene.cs
+++ Assets/Scripts/Scene/ResetScene.cs
@@ -7,6 +7,10 @@ public class ResetScene : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //GameObject.Find("Player").GetComponent<PlayerHealth>().Die();
+        PlayerPrefs.SetInt("Health", 5);
+        PlayerPrefs.SetInt("Coins", 0);
+        PlayerPrefs.SetInt("Souls", 0);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
