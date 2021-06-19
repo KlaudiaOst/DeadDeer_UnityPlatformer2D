1cf282da8b5db261420cdba107ad63e364b2bf88
KlaudiaOst <80642219+KlaudiaOst@users.noreply.github.com>
2021-06-19 18:18:49 +0200

    Dialog koniec

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -42,12 +42,19 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
     }
 
     IEnumerator TypeSentence (string sentence)
     {
-        dialogueText.test = "";
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+
     }
 
     void EndDialogue()
