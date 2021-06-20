7852d6bc5e15ba630878abb766cb19eb75a3d253
KlaudiaOst <80642219+KlaudiaOst@users.noreply.github.com>
2021-06-20 21:52:30 +0200

    jump
    mała poprawa

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour {
     public float speed = 10;
     public float jumpForce = 800;
@@ -94,6 +95,7 @@ public class PlayerMovement : MonoBehaviour {
             extraJumps--;
             
         }else if(Input.GetKeyDown(KeyCode.Space) && isGrounded == false && extraJumps == 0) {
+            rb.velocity = new Vector2(0, 0);
             rb.AddForce(new Vector2(0, jumpForce));
             extraJumps--;
         }
